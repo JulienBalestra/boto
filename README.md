14fd09cf1d83ca60a3c6328706306148a97458ba
Julien Balestra <julien.balestra@gmail.com>
2016-04-23 19:55:05 +0200

    Add polling capacity

--- README.md
+++ README.md
@@ -1,3 +1,74 @@
 # CoreOS Publisher
 
 Publish fresh etcd content get from https://discovery.etcd.io
+
+## How
+
+Run the python file `./s3_publisher.py` with the following requirements:
+
+Environment:
+* AWS_ID=ID
+* AWS_SECRET=SECRET
+
+
+Arguments values:
+* int size
+* str bucket_name
+
+Or use the docker container:
+
+
+    docker run --rm julienbalestra/coreos_publisher -h
+    
+    docker run --rm \
+        -e AWS_ID=ID \
+        -e AWS_SECRET=SECRET \
+         julienbalestra/coreos_publisher 3 bucket_name
+
+
+    docker run --rm julienbalestra/coreos_publisher -h
+    
+    docker run --rm \
+        -e AWS_ID=ID \
+        -e AWS_SECRET=SECRET \
+         julienbalestra/coreos_publisher 3 bucket_name
+         
+Will produce:
+
+    {
+        "url": "https://discovery.etcd.io/75859661e9178cea34d27d47fcc91587", 
+        "url_registered": [], 
+        "url_ts": "1461431876", 
+        "size": "3"
+    }
+
+This content is uploaded to your S3 bucket.
+
+    Uploading discovery_etcd.json
+    Upload done: http://coreos-deploy.s3-website-eu-west-1.amazonaws.com/discovery_etcd.json
+
+
+Depending on the --poll delay (default at 600s), this will notify you:
+
+    Polling https://discovery.etcd.io/75859661e9178cea34d27d47fcc91587 during 600s...
+    56s:	ec027cb438df449d806868a68f8fa29f=http://192.168.1.4		:2380
+    59s:	bc2e9b4258b1486ebe8c7199d6376f6a=http://192.168.1.37	:2380
+    125s:	eb355be3db93403e8c4a3f4cb8fa38cd=http://192.168.1.32	:2380
+    
+    Etcd 3/3 after 127s
+    Discovery object: 
+    {
+        "url": "https://discovery.etcd.io/75859661e9178cea34d27d47fcc91587", 
+        "url_registered": [
+            "ec027cb438df449d806868a68f8fa29f=http://192.168.1.4:2380", 
+            "bc2e9b4258b1486ebe8c7199d6376f6a=http://192.168.1.37:2380", 
+            "eb355be3db93403e8c4a3f4cb8fa38cd=http://192.168.1.32:2380"
+        ], 
+        "url_ts": "1461431876", 
+        "size": "3"
+    }
+
+Again, content is uploaded to your S3 bucket, replacing the **url_registered** value.
+
+    Uploading discovery_etcd.json
+    Upload done: http://coreos-deploy.s3-website-eu-west-1.amazonaws.com/discovery_etcd.json
--- s3_publisher.py
+++ s3_publisher.py
@@ -10,39 +10,54 @@ from boto.s3.key import Key
 
 
 class CoreOSPublisher(object):
+	peers_port = 2380
+	poller_steps = 2
 	base_discovery_etcd = "https://discovery.etcd.io/new?size="
-	discovery_url_file = "discovery_etcd.json"
+	discovery_file = "discovery_etcd.json"
 
-	def __init__(self, size, aws_id, aws_secret, bucket_name):
+	def __init__(self, size, aws_id, aws_secret, bucket_name, poll_delay=0):
 		self.size = size
 		self.new_discovery_url = ""
 		self.aws_id = aws_id
 		self.aws_secret = aws_secret
 		self.bucket_name = bucket_name
+		self.bucket_data = {}
+		self.poll_delay = poll_delay
+		self.registered = []
+
+	@staticmethod
+	def write_data(bucket_data, where):
+		print "Discovery object: "
+		print json.dumps(bucket_data, indent=4)
+
+		with open(where, 'w') as f:
+			json.dump(bucket_data, f)
 
 	def get_new_discovery_url(self):
-		req = requests.get("%s%s" % (self.base_discovery_etcd, self.size))
+		print "Request new etcd endpoint"
+		req = requests.get("%s%d" % (self.base_discovery_etcd, self.size))
 		self.new_discovery_url = req.content
 		url_ts = "%d" % time.time()
-		content = {
+		self.bucket_data = {
 			"url": "%s" % self.new_discovery_url,
-			"size": "%s" % self.size,
-			"url_ts": url_ts
+			"size": "%d" % self.size,
+			"url_ts": url_ts,
+			"url_registered": self.registered
 		}
 
-		print "New discovery object: "
-		print json.dumps(content, indent=4)
-
-		with open(self.discovery_url_file, 'w') as f:
-			json.dump(content, f)
-
-	def upload_discovery(self):
+	def upload_discovery_file(self):
+		self.write_data(self.bucket_data, self.discovery_file)
+		print "\nUploading %s ..." % self.discovery_file
 		conn = S3Connection(self.aws_id, self.aws_secret)
 		bucket = conn.get_bucket(self.bucket_name)
 		new_key = Key(bucket)
-		new_key.key = "%s" % self.discovery_url_file
-		new_key.set_contents_from_filename(self.discovery_url_file)
-		bucket.set_acl("public-read", self.discovery_url_file)
+		new_key.key = "%s" % self.discovery_file
+		new_key.set_contents_from_filename(self.discovery_file)
+		bucket.set_acl("public-read", self.discovery_file)
+		print "Upload done: http://{bucket_name}.s3-website-{bucket_location}.amazonaws.com/{discovery_file}\n".format(
+				bucket_name=self.bucket_name,
+				bucket_location=bucket.get_location(),
+				discovery_file=self.discovery_file)
 
 	def check(self):
 		if self.aws_id is None:
@@ -50,21 +65,56 @@ class CoreOSPublisher(object):
 		if self.aws_secret is None:
 			raise AttributeError("missing AWS_SECRET")
 
+	def poll_etcd(self):
+		print "Polling %s during %ds..." % (self.new_discovery_url, self.poll_delay)
+		start = time.time()
+		until = start + self.poll_delay
+		member_list = []
+		while len(member_list) != self.size and time.time() < until:
+			registered = requests.get("%s" % self.new_discovery_url)
+			try:
+				nodes = json.loads(registered.content)["node"]["nodes"]
+				for n in nodes:
+					value = n["value"]
+					if value not in member_list:
+						print "%ds:\t" % (time.time() - start), value.replace(
+								":%d" % self.peers_port, "\t:%d" % self.peers_port)
+						member_list.append(value)
+			except KeyError:
+				pass
+			time.sleep(self.poller_steps)
+
+		if len(member_list) == self.size:
+			print "\nEtcd %d/%d after %ds" % (
+				len(member_list), self.size, time.time() - start)
+			self.bucket_data["url_registered"] = member_list
+			self.upload_discovery_file()
+		else:
+			print "Polling timeout"
+
 
 def fast_arg_parsing():
 	args = argparse.ArgumentParser()
-	args.add_argument("size", type=str, help="Etcd cluster size")
+	args.add_argument("size", type=int, help="Etcd cluster size")
 	args.add_argument("bucket_name", type=str, help="Amazon Web Services S3 bucket")
-	return args.parse_args().size, args.parse_args().bucket_name
+	args.add_argument("--poll", default=600, type=int, help="Polling delay to follow registers")
+	return args.parse_args().size, args.parse_args().bucket_name, args.parse_args().poll
 
 
 if __name__ == "__main__":
 	# Fetch args
-	size, bucket_name = fast_arg_parsing()
-	aws_id = os.getenv("AWS_ID")
-	aws_secret = os.getenv("AWS_SECRET")
+	av_size, av_bucket_name, av_poll_delay = fast_arg_parsing()
+	env_aws_id = os.getenv("AWS_ID")
+	env_aws_secret = os.getenv("AWS_SECRET")
 
-	publisher = CoreOSPublisher(size, aws_id, aws_secret, bucket_name)
+	publisher = CoreOSPublisher(
+			av_size,
+			env_aws_id,
+			env_aws_secret,
+			av_bucket_name,
+			av_poll_delay)
+
 	publisher.check()
 	publisher.get_new_discovery_url()
-	publisher.upload_discovery()
+	publisher.upload_discovery_file()
+	publisher.poll_etcd()
